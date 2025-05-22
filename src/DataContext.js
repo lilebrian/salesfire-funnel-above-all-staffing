f88ac075f8b29a023250ed588ca57db0f6685580
lilebrian <brian.lile@salesfireconsulting.com>
2025-05-22 05:22:05 -0400

    Update DataContext.js

--- src/DataContext.js
+++ src/DataContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useEffect, useState } from 'react';
 const DataContext = createContext();
 export const useData = () => useContext(DataContext);
 
-const API_URL = "https://v1.nocodeapi.com/lilebrian/google_sheets/PhVOReqOSbWgtVFA?tabId=Above All Staffing";
+const API_URL = "https://v1.nocodeapi.com/lilebrian/google_sheets/PhVOReqOSbWgtVFA?tabId=Above%20All%20Staffing";
 
 export function DataProvider({ children }) {
   const [data, setData] = useState({});
